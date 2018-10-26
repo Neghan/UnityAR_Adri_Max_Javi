8fe1f1e26a64666a387af9ce4f5d861e7201019e
Max <massimoboettesta@enti.cat>
2018-10-26 11:08:15 +0200

    FirstCommit_until_Panel

---
+++ 3D Model AR/Assets/Scripts/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public static UIManager instance;
+
+    public GameObject actionBarPanel;
+
+    // Called when the script instance is loaded
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EnterFullScreen()
+    {
+        StartCoroutine(HideActionBarWithTimeout());
+    }
+
+    private IEnumerator HideActionBarWithTimeout()
+    {
+        actionBarPanel.GetComponent<Animator>().Play("Hide");
+        yield return new WaitForSeconds(3);
+        actionBarPanel.GetComponent<Animator>().Play("Show");
+    }
+}
